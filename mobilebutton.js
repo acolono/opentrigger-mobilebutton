5031a58ab777a8475b3e02b9f5f980b75363634a
git <@>
2016-10-10 18:15:56 +0000

    js version

--- mobilebutton.js
+++ mobilebutton.js
@@ -1,13 +1,29 @@
 ;jQuery(document).ready(function($){
 	"use strict";
-	if(window.request && window.request.hookUrl){
-		var hookUrl = window.request.hookUrl;
+	
+	var parseUrl = function(){
+    var match,
+        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+        query  = window.location.search.substring(1);
+
+    var urlParams = {};
+    while (match = search.exec(query))
+			urlParams[decode(match[1])] = decode(match[2]);
+		return urlParams;
+	};
+	
+	var request = parseUrl();
+	
+	if(request && request.hookUrl){
+		var hookUrl = request.hookUrl;
 		var $body = $('.body').first();
 		var $span = $body.find('>span').first();
 		
 		$.ajax({
 			url: hookUrl,
-			data: window.request,
+			data: request,
 			dataType: 'jsonp', 
 			success: function(data,status,xhr){  
 				$body.addClass('success');
